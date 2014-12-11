76ecde468266397bd5bfb30cfbb3497be49e5cdf
sarath280 <myappleid2849@gmail.com>
2014-12-11 21:33:57 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,28 +1,18 @@
-# Copyright (C) 2014 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/zopo/zp998/full_zp998.mk)
+# Release name
+PRODUCT_RELEASE_NAME := lcsh92_wet_jb9
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+# Inherit device configuration
+$(call inherit-product, device/alps/lcsh92_wet_jb9/device_lcsh92_wet_jb9.mk)
 
-PRODUCT_NAME := cm_zp998
-PRODUCT_DEVICE := zp998
-PRODUCT_MODEL := ZOPO ZP998
-PRODUCT_MANUFACTURER := ZOPO
-PRODUCT_RELEASE_NAME := ZP998
-PRODUCT_BRAND := Android
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := lcsh92_wet_jb9
+PRODUCT_NAME := cm_lcsh92_wet_jb9
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := lcsh92_wet_jb9
+PRODUCT_MANUFACTURER := alps
